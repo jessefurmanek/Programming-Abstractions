27ccf71d5e6960ab3bd9d6d738f1b0bb89479dfd
jessefurmanek <jesse.furmanek@gmail.com>
2013-10-14 17:31:41 -0500

    Instant Insanity Code
    Included instant insanity puzzle.  I'm pretty sure this works, but I've
    had a hard time checking my answer.

--- Chapter_7/main.cpp
+++ Chapter_7/main.cpp
@@ -8,17 +8,18 @@
 #include "console.h"
 #include "simpio.h"
 #include "grid.h"
-#include "nQueens.h"
+
 #include "vector.h"
 #include "InstantInsanity.h"
+
 
 void RunInsanityCubes();
 
 using namespace std;
 
 int main() {
-   
-    RunInsanityCubes();
-
+    
+        RunInsanityCubes();
+    
     return 0;
 }
