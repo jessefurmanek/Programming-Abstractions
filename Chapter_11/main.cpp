d32cde202dfbc11ea3d4cbf9079a9b0092b3b107
jessefurmanek <jesse.furmanek@gmail.com>
2014-02-16 16:32:08 -0600

    Finished problem 6 for my purposes.
    Couldn't get alternative operator function for using brackets with a
    vector e.g. vector[x] would return the x position in the vector.  But
    C++ can with this one.

--- Chapter_11/main.cpp
+++ Chapter_11/main.cpp
@@ -17,9 +17,39 @@
 using namespace std;
 
 int main() {
-
+    VectorTwoStx <string> testVec;
     
-
+    testVec.add("A");
+    testVec.add("B");
+    testVec.add("C");
+    
+    
+    testVec.removeAt(1);
+    testVec.insertAt(1, "X");
+    testVec.insertAt(1, "Y");
+    testVec.insertAt(1, "Z");
+    testVec.insertAt(1, "8");
+    
+    testVec.add("Z");
+    
+    testVec.clear();
+    
+    
+    testVec.add("A");
+    testVec.add("B");
+    testVec.add("C");
+    
+    testVec.setAt(0, "Z");
+    testVec.add("Y");
+    
+    
+    for(int x=0; x<testVec.size(); x++){
+        cout<<testVec[x]<<endl;
+    }
+   
+    
+    
+   
     return 0;
 }
 
--- Chapter_11/vector2stacks.h
+++ Chapter_11/vector2stacks.h
@@ -75,47 +75,92 @@ inline int VectorTwoStx<ElemType>::size() {
 template <typename ElemType>
 bool VectorTwoStx<ElemType>::isEmpty() {
     
-    return (stackone.size()+stacktwo.size()==0)
+    return (stackone.size()+stacktwo.size()==0);
 }
 
 template <typename ElemType>
 void VectorTwoStx<ElemType>::clear() {
-    for(int x=0; x<stackone.size(); x++){  //pop off all elements of stackone
-        stackone.pop;
+    int stackoneInitsize = stackone.size();
+    int stacktwoInitsize = stacktwo.size();
+    
+    for(int x=0; x<stackoneInitsize; x++){  //pop off all elements of stackone
+        stackone.pop();
     }
     
-    for(int x=0; x<stacktwo.size(); x++){
-        stacktwo.pop;                       //pop off all elemetns of stacktwo
+    for(int x=0; x<stacktwoInitsize; x++){
+        stacktwo.pop();                       //pop off all elemetns of stacktwo
     }
  
 }
 
 template <typename ElemType>
 ElemType VectorTwoStx<ElemType>::getAt(int index) {
+    if(index<0 || index>=(stackone.size()+stacktwo.size())){
+        error("index is not within a valid range");
+    }
     
+    int  stackoneInitsize = stackone.size();
+    
+    if(index<stackone.size()){
+        for(int x=1; x<stackoneInitsize-index; x++){  /*x=1 is used to compensate for the fact that the largest size will always be one greater than the largest index in stack one */
+            stacktwo.push(stackone.pop());  //move excess stack one letters to stack two
+        }
+        return stackone.peek();
+    }else{
+        for(int x=0; x<(index-stackoneInitsize); x++){
+            stackone.push(stacktwo.pop());  //move excess stack two letters to stack one
+        }
+        
+        return stacktwo.peek();
+    }
    
-    
+    error("index is not within a valid range");
+    return 0;  //because no one likes a compile error
 }
 
 template <typename ElemType>
 void VectorTwoStx<ElemType>::setAt(int index, ElemType elem) {
+    if(index<0 || index>=(stackone.size()+stacktwo.size())){
+        error("index is not within a valid range");
+    }
     
+    int  stackoneInitsize = stackone.size();
+    
+    if(index<stackone.size()){
+        for(int x=1; x<stackoneInitsize-index; x++){  /*x=1 is used to compensate for the fact that the largest size will always be one greater than the largest index in stack one */
+            stacktwo.push(stackone.pop());  //move excess stack one letters to stack two
+        }
+        stackone.pop();  //remove requested index
+        stackone.push(elem); //set requested index
+    }else{
+        for(int x=0; x<(index-stackoneInitsize); x++){
+            stackone.push(stacktwo.pop());  //move excess stack two letters to stack one
+        }
+        
+        stacktwo.pop();  //remove requested index
+        stacktwo.push(elem); //set requested index
+    }
 
     
 }
 
 template <typename ElemType>
 void VectorTwoStx<ElemType>::insertAt(int index, ElemType elem) {
+    if(index<0 || index>=(stackone.size()+stacktwo.size())){
+        error("index is not within a valid range");
+    }
     
+    int  stackoneInitsize = stackone.size();
+   
     if(index<stackone.size()){
-        for(int x=0; x<(stackone.size()-index); x++){
+        for(int x=0; x<(stackoneInitsize-index); x++){
         stacktwo.push(stackone.pop());     //if the index is in stack one, move every element including the inserted at element to stack two
         }
         
         stackone.push(elem);
     }else{  /* if the index value is greater or equal to the size of the index, the index is going to be located on the second stack.  therefore, move excess elements from stack 2 to get to the referenced index.
              */
-        for(int x=0; x<index-stackone.size(); x++){  /* subtract index from stack 1 size to reset index count on stack2.  meaning, if the index is on the top of the second stack, the index value will be equal to the size of stack1, as stack1 end on a value one less than the size of the stack.  this means if the index value is equal to the top of stack 2, subtracting the size of stack1 will not pop any values off of stack2.  the element will then be pushed to stack1, in effect inserting in constant time.... i think.
+        for(int x=0; x<(index-stackoneInitsize); x++){  /* subtract index from stack 1 size to reset index count on stack2.  meaning, if the index is on the top of the second stack, the index value will be equal to the size of stack1, as stack1 end on a value one less than the size of the stack.  this means if the index value is equal to the top of stack 2, subtracting the size of stack1 will not pop any values off of stack2.  the element will then be pushed to stack1, in effect inserting in constant time.... i think.
                 */
             stackone.push(stacktwo.pop());
         }
@@ -128,24 +173,36 @@ void VectorTwoStx<ElemType>::insertAt(int index, ElemType elem) {
 
 template <typename ElemType>
 void VectorTwoStx<ElemType>::removeAt(int index) {
-
+    if(index<0 || index>=(stackone.size()+stacktwo.size())){
+        error("index is not within a valid range");
+    }
+    
+    if(index<stackone.size()){
+        for(int x=1; x<stackone.size()-index; x++){  /*x=1 is used to compensate for the fact that the largest size will always be one greater than the largest index in stack one */
+            stacktwo.push(stackone.pop());  //move excess stack one letters to stack two
+        }
+        stackone.pop();  //remove requested index
+    }else{
+        for(int x=0; x<(stackone.size()-index); x++){
+            stackone.push(stacktwo.pop());  //move excess stack two letters to stack one
+        }
+        
+        stacktwo.pop();  //remove requested index
+    }
 }
 
 template <typename ElemType>
 void VectorTwoStx<ElemType>::add(ElemType elem) {
     
-    for(int x=0; x<stacktwo.size(); x++){
+    int stacktwoInitsize=stacktwo.size();
+    
+    for(int x=0; x<stacktwoInitsize; x++){
         stackone.push(stacktwo.pop());   //put all of stacktwos elements onto stackone
     }
 
     stackone.push(elem); //push the new element onto stackone
     
+    
 }
-
-template <typename ElemType>
-ElemType & VectorTwoStx<ElemType>::operator[](int index) {
-
-}
-
 
 #endif /* defined(__Ch_11_Coursework__vector2stacks__) */
