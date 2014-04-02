c7154cefce94d97f6d13c2c750948118821b2630
jessefurmanek <jesse.furmanek@gmail.com>
2014-04-01 19:10:45 -0500

    Finished problem 9-- added generic type quicksort method

--- Chapter_12/main.cpp
+++ Chapter_12/main.cpp
@@ -21,34 +21,13 @@ template <typename Type>
 void Sort(Type array[], int n, int (*cmp)(Type, Type)=OperatorCmp);  //defaulting the generic sorting fucntion to the operatorcmp function
 
 
+template <typename Type>
+int Partition(Type array[], int start, int finish);
+
+template <typename Type>
+void Quicksort(Type array[], int start, int finish);
 
 int main(){
-    Map<int> test;
-    Map<int> copytest;
-    
-    Map<int> *testArray = new Map<int>[1];
-    testArray[0] = test;
-    
-    for (int x = 0; x<10; x++){
-        test.put("test"+integerToString(x), x);
-    }
-    
-    test.copyEntriesInto(copytest);
-    
-    for (int x = 0; x<10; x++){
-        test.put("test"+integerToString(x), 2*x);
-    }
-    
-    
-    
-    for (int x = 0; x<10; x++){
-        cout<<copytest.get("test"+integerToString(x))<<endl;
-    }
-    
-    for (int x = 0; x<10; x++){
-        cout<<test.get("test"+integerToString(x))<<endl;
-    }
-   
     
     int *array = new int[5];
     array[0] = 5;
@@ -57,13 +36,17 @@ int main(){
     array[3] = 14;
     array[4] = 51;
     
-    
     Sort(array, 5);
+    
+    for (int x = 0; x<5; x++){
+        
+        cout<<array[x]<<endl;
+    }
     
     return 0;
 }
 
-template <typename Type>  //generic comparing function
+template <typename Type>  //generic comparing function for any "Type"
 int OperatorCmp(Type one, Type two) {
     if (one == two) return 0;
     if (one < two) return -1;
@@ -71,13 +54,35 @@ int OperatorCmp(Type one, Type two) {
 }
 
 template <typename Type>
-void Sort(Type array[], int n, int (*cmp)(Type, Type)) {
-    for (int i = 0; i < n; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (cmp(array[j],array[minIndex]) < 0) minIndex = j;  //because OperatorCmp is set as the default compare function, calling *cmp
-        }                                                          //calls the OperatorCmp function
-        Type temp =array[i];
-        array[i] = array[minIndex];
-        array[minIndex] = temp;
-    } }
+void Sort(Type array[], int n, int (*cmp)(Type, Type)) { //Sort acts as a wrapper for the quicksort alogrithm
+    Quicksort(array, 0, n-1);
+}
+
+template <typename Type>
+void Quicksort(Type array[], int start, int finish) {
+    if (start >= finish) return;
+    int boundary = Partition(array, start, finish);
+    Quicksort(array, start, boundary - 1);
+    Quicksort(array, boundary + 1, finish);
+}
+
+
+template <typename Type>
+int Partition(Type array[], int start, int finish) {
+    int pivot = array[start];
+    int lh = start + 1;
+    int rh = finish;
+    while (true) {
+        while (lh < rh && array[rh] >= pivot) rh--;
+        while (lh < rh && array[lh] < pivot) lh++;
+        if (lh == rh) break;
+        Type temp = array[lh];
+        array[lh] = array[rh];
+        array[rh] = temp;
+    }
+    if (array[lh] >= pivot) return start;
+    array[start] = array[lh];
+    array[lh] = pivot;
+    return lh;
+}
+
